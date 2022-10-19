2eafe26f0602236164910523312a1fd4513bb9bd
qwer0114 <112809788+qwer0114@users.noreply.github.com>
2022-10-19 16:11:29 +0900

    javascript

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const img = [
+    "img01.jpeg", "img02.jpeg", "img03.jpeg"
+]
+
+const todayImage = img[Math.floor(Math.random() * img.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = "img/" + todayImage;
+
+document.body.appendChild(bgImage);
---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "36ea50756d0c000c9672449ae39603b1";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:frist-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        weather.innerText = data.weather[0].main;
+        city.innerText = data.name;
+    });
+
+}
+function onGeoError() {
+    alert("Can't find you");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
